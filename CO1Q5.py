d843cfc17283dd3d0041660b318101c562e7bff7
Rakesh6238 <125358983+Rakesh6238@users.noreply.github.com>
2023-02-14 22:29:40 +0530

    Delete CO1Q5.py

--- CO1Q5.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter the limit:"))
-for i in range(1,n+1):
-    for j in range(0,i+1):
-        a=i*j
-        if a==0:
-            continue
-        else:
-            print(a,end=" ")
-    print("\n")
