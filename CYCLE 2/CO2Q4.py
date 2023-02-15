f01c7690ccc2cd99cb0feac374241d150743cf38
Rakesh6238 <125358983+Rakesh6238@users.noreply.github.com>
2023-02-15 12:39:24 +0530

    Add files via upload

---
+++ CYCLE 2/CO2Q4.py
@@ -0,0 +1,7 @@
+from math import sqrt
+list=[]
+n=int(input("enter the limit:"))
+for i in range(1000,n):
+    if sqrt(i)==int(sqrt(i)) and i%2==0:
+        print(i)
+    
---
+++ CYCLE 2/CO2Q6.py
@@ -0,0 +1,8 @@
+str=input("enter the string:")
+c=0
+for i in str:
+    if i==" ":
+        continue
+    else:
+        c+=1
+print("charaters in "+str,"is",c)        
